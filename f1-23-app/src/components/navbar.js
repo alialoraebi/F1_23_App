fd5d676af82a1d36689185a8c5d13048eec91426
David Anton <davidanton037@gmail.com>
2024-04-22 12:25:21 -0400

    Redone the Navbar

--- f1-23-app/src/components/navbar.js
+++ f1-23-app/src/components/navbar.js
@@ -1,37 +1,43 @@
-import React, { useContext} from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from './AuthContext'; 
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "./AuthContext";
 
 function Navbar() {
-  const { isLoggedIn, logOut } = useContext(AuthContext); 
+  const { isLoggedIn, logOut } = useContext(AuthContext);
 
   const handleLogOut = () => {
-    if (window.confirm('Are you sure you want to log out?')) {
-      logOut(); 
-      localStorage.removeItem('token'); 
-      localStorage.removeItem('userId'); 
+    if (window.confirm("Are you sure you want to log out?")) {
+      logOut();
+      localStorage.removeItem("token");
+      localStorage.removeItem("userId");
     }
   };
 
   return (
-    <header className="bg-gray-800 text-white py-4">
-      <div className="container max-w-full flex justify-between items-center">
-        <div className="ml-8">
-          <h1 className="text-xl font-bold">F1 23 Stats</h1>
+    <header>
+    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+            <a href="/" class="flex items-center">
+                <img src="https://cdn-icons-png.flaticon.com/512/2418/2418779.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">F1 23 Stats</span>
+            </a>
+            <div class="flex items-center lg:order-2">
+                {!isLoggedIn ? (
+                  <>
+                    <Link to="/signup" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Sign Up</Link>
+                    <Link to="/login" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</Link>
+                  </>
+                  ) : (
+                    <button onClick={handleLogOut} className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Logout</button>
+                )}
+                <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                    <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+            </div>
         </div>
-        <div className="ml-auto">
-          <nav className='mr-8'>
-            {!isLoggedIn ? (
-              <>
-                <Link to="/signup" className="text-white px-4 py-2 rounded hover:bg-gray-700 transition-colors">Sign Up</Link>
-                <Link to="/login" className="text-white px-4 py-2 rounded hover:bg-gray-700 transition-colors">Login</Link>
-              </>
-            ) : (
-              <button onClick={handleLogOut} className="text-white px-4 py-2 rounded hover:bg-red-600 transition-colors">Logout</button>
-            )}
-          </nav>
-        </div>
-      </div>
+    </nav>
     </header>
   );
 }
